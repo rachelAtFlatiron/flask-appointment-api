7ea27a91264e7b1541b0b5dd3d7b5bda441b24c4
d-gaston <dgaston@udel.edu>
2024-01-11 16:53:07 -0500

    solution

--- app.py
+++ app.py
@@ -21,7 +21,70 @@ def index():
     return "doctor/patient"
 
 
+@app.get("/doctors")
+def get_doctors():
+    doctors = Doctor.query.all()
+    return [d.to_dict(rules=["-appointments"]) for d in doctors], 200
 
+
+@app.get("/doctors/<int:id>")
+def get_doctor_by_id(id):
+    doctor = db.session.get(Doctor, id)
+    if not doctor:
+        return {"error": "not found"}, 404
+    return (
+        doctor.to_dict(rules=["-appointments.patient_id", "-appointments.doctor_id"]),
+        200,
+    )
+
+
+@app.get("/patients/<int:id>")
+def get_patient_by_id(id):
+    patient = db.session.get(Patient, id)
+    if not patient:
+        return {"error": "not found"}, 404
+    patient_dict = patient.to_dict(rules=['-appointments'])
+    doctor_dicts = [d.to_dict(rules=['-appointments']) for d in patient.doctors]
+    patient_dict["doctors"] = doctor_dicts
+    return patient_dict, 200
+
+@app.patch('/patients/<int:id>')
+def patch_patient(id):
+    try:
+        data = request.json
+        patient = db.session.get(Patient, id)
+        if not patient:
+            return {"error": "not found"}, 404
+        for key in data:
+            setattr(patient, key, data[key])
+        db.session.add(patient)
+        db.session.commit()
+        return patient.to_dict(rules=['-appointments'])
+    except Exception as e:
+        return {"error": str(e)}, 404
+
+@app.post('/appointments')
+def post_appointments():
+    try:
+        data = request.json
+        appointment = Appointment(day=data.get('day'), patient_id=data.get('patient_id'), doctor_id=data.get('doctor_id'))
+        db.session.add(appointment)
+        db.session.commit()
+        return appointment.to_dict(rules=['-doctor_id', '-patient_id'])
+    except Exception as e:
+        return {"error": str(e)}, 404
+        
+
+@app.post("/doctors")
+def post_doctor():
+    try:
+        data = request.json
+        doctor = Doctor(name=data.get("name"), specialty=data.get("specialty"))
+        db.session.add(doctor)
+        db.session.commit()
+        return doctor.to_dict(), 201
+    except Exception as e:
+        return {"error": str(e)}
 
 
 if __name__ == "__main__":
--- models.py
+++ models.py
@@ -15,17 +15,48 @@ db = SQLAlchemy(metadata=metadata)
 
 class Patient(db.Model, SerializerMixin):
     __tablename__ = "patient_table"
+    serialize_rules = ['-appointments.patient']
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
 
+    appointments = db.relationship("Appointment", back_populates="patient")
 
+    # doctors = [a.doctor for a in appointments]
+    doctors = association_proxy("appointments", "doctor")
 
 class Appointment(db.Model, SerializerMixin):
     __tablename__ = "appointment_table"
+    serialize_rules = ['-patient.appointments', '-doctor.appointments']
     id = db.Column(db.Integer, primary_key=True)
+    day = db.Column(db.String, nullable=False)
+    doctor_id = db.Column(db.Integer, db.ForeignKey("doctor_table.id"), nullable=False)
+    patient_id = db.Column(
+        db.Integer, db.ForeignKey("patient_table.id"), nullable=False
+    )
 
-
+    patient = db.relationship("Patient", back_populates="appointments")
+    doctor = db.relationship("Doctor", back_populates="appointments")
+    @validates('day')
+    def validate_day(self, key, day):
+        weekdays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+        if day not in weekdays:
+            raise ValueError("Doctors don't work on weekends here")
+        return day
 
 
 class Doctor(db.Model, SerializerMixin):
     __tablename__ = "doctor_table"
+    serialize_rules = ['-appointments.doctor']
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    specialty = db.Column(db.String, nullable=False)
+    appointments = db.relationship("Appointment", back_populates="doctor")
+    
+
+    patients = association_proxy("appointments", "patient")
+    # name = validate_name("name", "Dr. Smith")
+    @validates("name")
+    def validate_name(self, key, name):
+        if not name.startswith("Dr."):
+            raise ValueError("Doctor name must start with Dr.")
+        return name
